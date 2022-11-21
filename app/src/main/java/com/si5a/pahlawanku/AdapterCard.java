b2ee332834095a1febc49e385b64dc322ef917f7
hannypaseputri <114385288+hannypaseputri@users.noreply.github.com>
2022-11-21 14:38:55 +0700

    21/11/2022

--- app/src/main/java/com/si5a/pahlawanku/AdapterCard.java
+++ app/src/main/java/com/si5a/pahlawanku/AdapterCard.java
@@ -1,5 +1,7 @@
 package com.si5a.pahlawanku;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +17,11 @@ import java.util.ArrayList;
 
 public class AdapterCard extends  RecyclerView.Adapter<AdapterCard.ClassViewHolder>{
     private ArrayList<ModelPahlawan> dataPahlawan;
+    private Context ctx;
 
-    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan, Context ctx) {
         this.dataPahlawan = dataPahlawan;
+        this.ctx = ctx;
     }
 
     @NonNull
@@ -33,10 +37,28 @@ public class AdapterCard extends  RecyclerView.Adapter<AdapterCard.ClassViewHold
         holder.tvNama.setText(pahlawan.getNama());
         holder.tvTentang.setText(pahlawan.getTentang());
         Glide
-                .with(holder.itemView.getContext())
+                .with(ctx)
                 .load(pahlawan.getFoto())
                 .centerCrop()
                 .into(holder.ivFoto);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String xNama, xTentang, xFoto;
+
+
+                xNama = pahlawan.getNama();
+                xTentang = pahlawan.getTentang();
+                xFoto = pahlawan.getFoto();
+
+                Intent kirim = new Intent(ctx, DetailActivity.class);
+                kirim.putExtra("xNama", xNama);
+                kirim.putExtra("xTentang", xTentang);
+                kirim.putExtra("xFoto", xFoto);
+                ctx.startActivity(kirim);
+            }
+        });
 
     }
 
